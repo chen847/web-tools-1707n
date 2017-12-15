8491b2cc7dc73e4e09298ea4cf9f07090cc158ab
thu <mingfei.net@gmail.com>
2017-12-15 10:29:43 +0800

    Update

---
+++ gulp-demo/gulpfile.js
@@ -0,0 +1,15 @@
+/**
+ * https://github.com/thu/web-tools-1707n
+ * Date: 2017/12/15 10:19
+ */
+
+// gulp 的配置文件
+
+// 1. 安装
+// 2. 引入
+const gulp = require('gulp');
+
+// 3. 定义一个 gulp 的任务 task
+gulp.task('default', () => {
+   console.log('Hello, gulp!');
+});
