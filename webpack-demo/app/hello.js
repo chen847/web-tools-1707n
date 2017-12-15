fa2b5cb9a1dbca421ec9f95c8bca382e13bf73a6
thu <mingfei.net@gmail.com>
2017-12-15 15:59:38 +0800

    Update

---
+++ webpack-demo/app/hello.js
@@ -0,0 +1,10 @@
+/**
+ * https://github.com/thu/web-tools-1707n
+ * Date: 2017/12/15 15:50
+ */
+
+exports = function () {
+  let hello = document.createElement('div');
+  hello.textContent = 'Hello, Webpack!';
+  return hello;
+};
---
+++ webpack-demo/app/main.js
@@ -0,0 +1,7 @@
+/**
+ * https://github.com/thu/web-tools-1707n
+ * Date: 2017/12/15 15:54
+ */
+
+const hello = require('./hello');
+document.querySelector('#root').appendChild(hello());
